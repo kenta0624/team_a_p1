5acce03c304073968af6f239480d01f32944f536
Taichi Furihata <a15@julyinc.com>
2016-10-05 16:00:57 +0900

    ユーザーをbakeしました

---
+++ app/Test/Fixture/UserFixture.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User Fixture
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 16, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum do',
+			'created' => '2016-10-05 06:54:15',
+			'modified' => '2016-10-05 06:54:15'
+		),
+	);
+
+}
