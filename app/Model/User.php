7330f9c050130b3f7af8a9f0b41b599c56eacdd4
AzureAD\a04 <a04@julyinc.com>
2016-10-12 10:42:43 +0900

    User Modelにログインしているユーザのidを取得するメソッドを追加。Authコンポーネントで、ログインしているユーザのidがうまく取れないため。

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,76 +1,79 @@
 <?php
 App::uses('AppModel', 'Model');
 App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
 /**
  * User Model
  *
  * @property Event $Event
  */
-class User extends AppModel {
+class User extends AppModel
+{
 
-/**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'name';
+    /**
+     * Display field
+     *
+     * @var string
+     */
+    public $displayField = 'name';
 
-/**
- * Validation rules
- *
- * @var array
- */
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
 
-	public $validate = array(
-		'name' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'password' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+    public $validate = array(
+        'name' => array(
+            'notBlank' => array(
+                'rule' => array('notBlank'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'password' => array(
+            'notBlank' => array(
+                'rule' => array('notBlank'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
 
-	// The Associations below have been created with all possible keys, those that are not needed can be removed
+    // The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * hasMany associations
- *
- * @var array
- */
-	public $hasMany = array(
-		'Event' => array(
-			'className' => 'Event',
-			'foreignKey' => 'user_id',
-			'dependent' => false,
-			'conditions' => '',
-			'fields' => '',
-			'order' => '',
-			'limit' => '',
-			'offset' => '',
-			'exclusive' => '',
-			'finderQuery' => '',
-			'counterQuery' => ''
-		)
-	);
+    /**
+     * hasMany associations
+     *
+     * @var array
+     */
+    public $hasMany = array(
+        'Event' => array(
+            'className' => 'Event',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        )
+    );
 
     /*
      *
      * */
-    public function beforeSave($options = array()) {
+    public function beforeSave($options = array())
+    {
         if (!empty($this->data[$this->alias]['password'])) {
             $passwordHasher = new SimplePasswordHasher(array('hashType' => 'sha256'));
             $this->data[$this->alias]['password'] = $passwordHasher->hash(
@@ -78,6 +81,13 @@ class User extends AppModel {
             );
         }
         return true;
+    }
+
+    public function getUserId()
+    {
+        $user =$this->find('first',array('conditions' => array('name' => AuthComponent::user()['User']['name'])));
+
+        return $user['User']['id'];
     }
 
 }
